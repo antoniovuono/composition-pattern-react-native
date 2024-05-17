e06ec60fa282a196ac9cd43fe913800fd9901711
Antonio Vuono <antoniosvuono@icloud.com>
2024-05-16 21:05:29 -0300

    fix: removing unessied importations

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
 import PrimaryHeader from "../../components/PrimaryHeader";
 import { Container } from "./styles";
 
