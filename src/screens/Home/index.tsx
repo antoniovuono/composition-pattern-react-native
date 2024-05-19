de4d9ebdd2119cc11d3daffa748e39ec246ac50f
Antonio Vuono <antoniosvuono@icloud.com>
2024-05-19 14:04:57 -0300

    feature: create variants on home scrren

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "../../components";
 import { ActionsButton } from "../../components/PrimaryHeader/styles";
 import PrimaryHeader from "../../components/PrimaryHeader";
 
+// variant 1
 export const Home = () => {
 	return (
 		<Container>
@@ -31,4 +32,42 @@ export const Home = () => {
 	);
 };
 
+// variant 2
+// export const Home = () => {
+// 	return (
+// 		<Container>
+// 			<Header.Box>
+// 				<Header.ActionsContainer>
+// 					<Header.ActionsButton onPress={() => console.log("goBack")}>
+// 						<Header.Icon iconName="arrow-back" />
+// 					</Header.ActionsButton>
+// 				</Header.ActionsContainer>
+// 			</Header.Box>
 
+// 		</Container>
+// 	);
+// };
+
+// variant 3
+// export const Home = () => {
+// 	return (
+// 		<Container>
+// 			<Header.Box>
+// 				<Header.ProfileDetailsContainer>
+// 					<Header.Avatar url="https://avatars.githubusercontent.com/u/7297243?s=96&v=4"/>
+
+// 					<Header.ProfileDetailsContent>
+// 						Bem vindo, Antonio
+// 					</Header.ProfileDetailsContent>
+// 				</Header.ProfileDetailsContainer>
+
+// 			<Header.ActionsContainer>
+// 				<ActionsButton onPress={() => console.log("add_click")}>
+// 					<Header.Icon iconName="add" />
+// 				</ActionsButton>
+// 			</Header.ActionsContainer>
+// 			</Header.Box>
+
+// 		</Container>
+// 	);
+// };
